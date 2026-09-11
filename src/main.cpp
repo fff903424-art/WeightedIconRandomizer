d789aa8d4181c894c28d6a582c41331eda813e62
fff903424-art <fff903424@gmail.com>
2026-09-10 19:12:01 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,16 +7,27 @@
 using namespace geode::prelude;
 
 class $modify(WIRPlayLayer, PlayLayer) {
-    void destroyPlayer(PlayerObject* player, GameObject* object) {
+    void destroyPlayer(
+        PlayerObject* player,
+        GameObject* object
+    ) {
         if (player && wir::isCubeMode(player)) {
-            log::info("Weighted Icon Randomizer: player died in cube mode.");
+            log::info(
+                "Weighted Icon Randomizer: cube death detected."
+            );
+
             wir::randomizeCube();
         }
 
-        PlayLayer::destroyPlayer(player, object);
+        PlayLayer::destroyPlayer(
+            player,
+            object
+        );
     }
 };
 
 $on_mod(Loaded) {
-    log::info("Weighted Icon Randomizer loaded successfully.");
+    log::info(
+        "Weighted Icon Randomizer loaded."
+    );
 }
